88c530c4b28b83eded2eae521039cab6b6d63418
Joey Kirby <jkirbymusic@gmail.com>
2017-02-09 15:46:31 -0600

    add comments

--- models/customer.py
+++ models/customer.py
@@ -1,6 +1,33 @@
 class Customer():
+    """
+    Customer class allows users to create a customer acct to make purchases with our CLI Tool.
+
+    Method List:   
+    - __init__ 
+    - get_customer_name
+    - get_street_address
+    - get_city
+    - get_state
+    - get_postal_code
+    - get_phone_number
+    - get_status
+    - set_status(self, new_status):
+
+    Author: Joey Kirby, Temporary Oysters
+    """
 
     def __init__(self, customer_name, street_address, city, state, postal_code, phone_number):
+        """
+        A new customer is created with required arguments.
+
+        Arg List:
+        - customer name
+        - street address
+        - city
+        - state
+        - postal_code
+        - phone_number
+        """
         self.customer_name = customer_name 
         self.street_address = street_address
         self.city = city
@@ -10,26 +37,41 @@ class Customer():
         self.status = False
 
     def get_customer_name(self):
+        """ Returns a customer's name """
         return self.customer_name
 
     def get_street_address(self):
+        """ Returns a customer's street address """
         return self.street_address
 
     def get_city(self):
+        """ Returns a customer's city """
         return self.city
 
     def get_state(self):
+        """ Returns a customer's state """
         return self.state
 
     def get_postal_code(self):
+        """ Returns a customer's postal code """
         return self.postal_code
 
     def get_phone_number(self):
+        """ Returns a customer's phone number """
         return self.phone_number
 
     def get_status(self):
+        """
+        Returns a customer's active status. True = Active User, False = Non-active User
+        Active User - Abilityto  make purchases
+        """
         return self.status
 
     def set_status(self, new_status):
+        """
+        Set a Users Active status to True or False. 
+        True = Active User, False = Non-active User
+        Active User - Abilityto  make purchases
+        """
         self.status = new_status
         return self.status
--- tests/TestCustomer.py
+++ tests/TestCustomer.py
@@ -4,15 +4,36 @@ sys.path.append("../")
 from models import customer
 
 class TestCustomer(unittest.TestCase):
+    """
+    TestCustomer tests the creation of a customer and it's methods.
+
+    Method List:   
+    -test_customer_acct_can_be_created
+    -test_customer_has_correct_attributes
+    -test_customer_can_change_active_status
+
+    Argument List:  unittest.TestCase gives the unittest model knowledge on what to test.
+
+    Author: Joey Kirby, Temporary Pysters
+    """
 
     @classmethod
     def setUpClass(self):
+        """
+        Sets up a Test User, "Joey", so we can TEST if our code is behaving as expected.
+        """
         self.joey = customer.Customer('Joey', '787 East Silver St', 'Lebanon', 'Ohio', '35622', '5551231234')
         
     def test_customer_acct_can_be_created(self):
+        """
+        Sets up a Test User, "Joey", so we can TEST if our code is behaving as expected.
+        """
         self.assertIsInstance(self.joey, customer.Customer)
 
     def test_customer_has_correct_attributes(self):
+        """
+        Tests that our users has created with the correct amount of attributes (6) & have value. 
+        """
         self.assertIsNotNone(self.joey.customer_name)
         self.assertIsNotNone(self.joey.street_address)
         self.assertIsNotNone(self.joey.city)
@@ -20,5 +41,20 @@ class TestCustomer(unittest.TestCase):
         self.assertIsNotNone(self.joey.postal_code)
         self.assertIsNotNone(self.joey.phone_number)
 
-#run in test dir
-#python -m unittest discover -s . -p "Test*.py" -v
+    def test_customer_can_change_active_status(self):
+        """
+        Tests that customer has ability to change active status. 
+        """
+        self.joey.status = True
+        self.assertTrue(self.joey.status)
+
+        self.joey.status = False
+        self.assertFalse(self.joey.status)
+
+'''
+run in CL within the test directory to RUN TESTS. 
+
+python -m unittest discover -s . -p "Test*.py" -v
+
+'''
+
